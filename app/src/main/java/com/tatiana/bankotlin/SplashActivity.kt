8c97ca07a10629bfb1a913bcd1454d22d227aa47
tatiana78 <tatiamagalhaes2014@gmail.com>
2020-03-28 23:18:57 -0300

    criado splash activity

--- app/src/main/java/com/tatiana/bankotlin/SplashActivity.kt
+++ app/src/main/java/com/tatiana/bankotlin/SplashActivity.kt
@@ -1,13 +1,18 @@
 package com.tatiana.bankotlin.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.tatiana.bankotlin.R
 
-class MainActivity : AppCompatActivity() {
+
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
+
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
